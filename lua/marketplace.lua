f0f394bb9c1351366fc495b28cf0cad92a1cacd4
xiaoshihou514 <onelessononeexercise@gmail.com>
2023-02-12 18:07:10 +0800

    chore: exploring the template

---
+++ lua/marketplace.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+function M.setup(opts)
+	-- configuration goes here
+end
+
+function M.toggle_view()
+	-- toggle the side search bar
+	-- maybe put some popular plugins on it on startup
+end
+
+return M
